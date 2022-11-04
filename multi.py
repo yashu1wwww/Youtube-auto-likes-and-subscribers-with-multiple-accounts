8a5212b538ebd32010a64f024b20f91b26effc7f
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-11-04 12:05:22 +0530

    Update multi.py

--- multi.py
+++ multi.py
@@ -22,19 +22,12 @@ with open("urls.txt") as f: #change url in text file
     for url in f:
         driver.get(url)
         
- time.sleep(5) #if video dont have ads then change 8 to 3 or 4       
+time.sleep(5) #if video dont have ads then change 8 to 3 or 4       
 driver.find_element_by_css_selector('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button').click()       
 driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/div[1]/ytd-segmented-like-dislike-button-renderer/div[1]/ytd-toggle-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]').click()
 time.sleep(2)
 driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[1]/div/ytd-subscribe-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]').click()
 driver.close()
-
-
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
 
 email = 'nonunkown@gmail.com \n'   #enter mail
 password = 'putpass\n'             #enter pass
@@ -60,6 +53,7 @@ driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-wat
 driver.close()
 
 
-#add these script again and again  i added only two you addd how much time like and sub you want and change mail and pass how much time you want in text file url is enough for all new drivers 
+#add these script again and again  i added only two you add how much time auto like and sub you want copy line from 32 to 53 and paste in 55th line and dont forgot to remove mail and password do these tricks if want more....
+#change url in url.txt to your required video link...
 
-#if you only want like and dont want auto subscribers means remove the 29 and 59 line which contain subscribe xpath code same
+#if you only want like and dont want auto subscribers means remove the 29 and 52 line which contain subscribe xpath code same..
